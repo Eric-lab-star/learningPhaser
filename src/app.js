d51f5e2c8bfba22c3057b1214a29efe6f6a5a3ed
Eric Kim <cyon256@icloud.com>
2022-06-23 00:56:14 +0900

    nodemon setup

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
+
 app.use(express.static("src/public"));
 
 app.get("/", (req, res) => {
   res.render("phaser");
-});
-
-app.get("/video", (req, res) => {
-  res.render("phaser.html");
 });
 
 app.listen(port, () => {
